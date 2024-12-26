3ac5d01f48c895855e5595713d662fbc5b64a0f2
GilangAndhika <gryzlegrizz@gmail.com>
2024-12-27 02:19:23 +0700

    Update media sources URLs

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -4,14 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     // Data gambar
     const imageData = [
         { src: "https://t.if.co.id/714220046/assets/img/logo-devteam.png", alt: "Desain Logo-Devteam" },
-        { src: "../../assets/img/PosterYudisium.png", alt: "Desain Poster-Yudisium" },
-        { src: "../../assets/img/flyer-bootcamp.png", alt: "Desain Flyer Bootcamp" },
-        { src: "../../assets/img/flyer-kulum.png", alt: "Desain Flyer Kuliah Umum" },
-        { src: "../../assets/img/idcard-yudis.png", alt: "Desain ID Card" },
-        { src: "../../assets/img/banner-yudis.png", alt: "Desain Banner" },
-        { src: "../../assets/img/clothes-design-whites.jpg", alt: "Desain Baju Putih" },
-        { src: "../../assets/img/short-film-thumbnail.jpg", alt: "Thumbnail Short Film" },
-        { src: "../../assets/img/clothes-design-blacks.jpg", alt: "Desain Baju Hitam" },
+        { src: "https://t.if.co.id/714220046/assets/img/PosterYudisium.png", alt: "Desain Poster-Yudisium" },
+        { src: "https://t.if.co.id/714220046/assets/img/flyer-bootcamp.png", alt: "Desain Flyer Bootcamp" },
+        { src: "https://t.if.co.id/714220046/assets/img/flyer-kulum.png", alt: "Desain Flyer Kuliah Umum" },
+        { src: "https://t.if.co.id/714220046/assets/img/idcard-yudis.png", alt: "Desain ID Card" },
+        { src: "https://t.if.co.id/714220046/assets/img/banner-yudis.png", alt: "Desain Banner" },
+        { src: "https://t.if.co.id/714220046/assets/img/clothes-design-whites.jpg", alt: "Desain Baju Putih" },
+        { src: "https://t.if.co.id/714220046/assets/img/short-film-thumbnail.jpg", alt: "Thumbnail Short Film" },
+        { src: "https://t.if.co.id/714220046/assets/img/clothes-design-blacks.jpg", alt: "Desain Baju Hitam" },
     ];
 
     // Mengisi galeri dengan gambar dari data
